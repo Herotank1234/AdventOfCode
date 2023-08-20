85d2e3734fb421978db1280a044f99920abf349f
Zhi Liang <zhikang8@hotmail.co.uk>
2023-08-20 17:13:14 +0100

    Skeleton files

---
+++ 2016/src/q23.cpp
@@ -0,0 +1,22 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+  std::ifstream ifstrm("./data/q_.txt", std::ios::in);
+  std::vector<int> data;
+
+  if(!ifstrm.is_open()) {
+    std::cout << "Failed to open file" << std::endl;
+  } else {
+    std::string line;
+    while(std::getline(ifstrm, line)) {
+      continue;
+    }
+  }
+
+  std::cout << "Answer to part 1: " << std::endl;
+  std::cout << "Answer to part 2: " << std::endl;
+  return 0;
+}
