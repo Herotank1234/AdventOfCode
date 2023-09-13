3ed024c698c0fcdd69fa2e4bb1ffc14c2bc319d3
Zhi Liang <zhikang8@hotmail.co.uk>
2023-09-13 15:07:10 +0100

    Day 1

--- 2021/src/q1.rs
+++ 2021/src/q1.rs
@@ -1,8 +1,32 @@
 use std::fs;
 
+fn larger_than_prev(weights: &Vec<u32>, window_size: usize) -> u32 {
+  let mut larger: u32 = 0;
+  for i in 0..weights.len() - window_size{
+    let mut curr_weight: u32 = 0;
+    let mut next_weight: u32 = 0;
+
+    for j in i..i + window_size {
+      curr_weight += weights[j];
+    }
+    for j in i + 1.. i + 1 + window_size {
+      next_weight += weights[j];
+    }
+
+    larger += if next_weight > curr_weight { 1 } else { 0 };
+  }
+
+  return larger;
+}
+
 fn main() {
-  let _contents = fs::read_to_string("./data/q_.txt")
+  let contents = fs::read_to_string("./data/q1.txt")
     .expect("Should have been able to read file");
-  println!("Part 1: {}", 0);
-  println!("Part 2: {}", 0);
+
+  let weights: Vec<u32> = contents.split('\n')
+    .map(|str| str.parse::<u32>().unwrap())
+    .collect::<Vec<u32>>();
+
+  println!("Part 1: {}", larger_than_prev(&weights, 1));
+  println!("Part 2: {}", larger_than_prev(&weights, 3));
 }
