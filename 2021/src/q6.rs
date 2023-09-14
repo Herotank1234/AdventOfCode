be8bb10336011fd57c6ead994a30d8c3df3dfac8
Zhi Liang <zhikang8@hotmail.co.uk>
2023-09-14 15:57:25 +0100

    Day 6

--- 2021/src/q6.rs
+++ 2021/src/q6.rs
@@ -1,8 +1,40 @@
 use std::fs;
+use std::collections::HashMap;
+
+fn count_final_lanternfish(initial_values: &Vec<u32>, days: u32) -> u64 {
+  let mut fish_timers: HashMap<u32, u64> = HashMap::new();
+
+  for i in 0..9 {
+    fish_timers.insert(i, 0);
+  }
+
+  for val in initial_values {
+    *(fish_timers.get_mut(val).unwrap()) += 1;
+  }
+
+  for _ in 0..days {
+    let mut next_fish_timers: HashMap<u32, u64> = HashMap::new();
+
+    for i in (0..8).rev() {
+      next_fish_timers.insert(i, *fish_timers.get(&(i + 1)).unwrap());
+    }
+
+    *next_fish_timers.get_mut(&6).unwrap() += *fish_timers.get(&0).unwrap();
+    next_fish_timers.insert(8, *fish_timers.get(&0).unwrap());
+    fish_timers = next_fish_timers;
+  }
+
+  return fish_timers.iter()
+    .fold(0, |a, (_, v)| a + v);
+}
 
 fn main() {
-  let _contents = fs::read_to_string("./data/q_.txt")
+  let contents = fs::read_to_string("./data/q6.txt")
     .expect("Should have been able to read file");
-  println!("Part 1: {}", 0);
-  println!("Part 2: {}", 0);
+  let initial_values = contents.split(',')
+    .map(|val| val.parse::<u32>().unwrap())
+    .collect::<Vec<u32>>();
+
+  println!("Part 1: {}", count_final_lanternfish(&initial_values, 80));
+  println!("Part 2: {}", count_final_lanternfish(&initial_values, 256));
 }
